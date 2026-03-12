fccefbe7a47579856d779c9e1453815e52c63d08
Wilmer Soto <wsoto04@gmail.com>
2026-03-12 18:15:54 -0500

    feat: enhance WhyHireMe section with dynamic translations and detailed description

--- src/components/organisms/WhyHireMeSection.tsx
+++ src/components/organisms/WhyHireMeSection.tsx
@@ -1,13 +1,15 @@
 import SectionHeader from "@/components/molecules/SectionHeader";
 import WhyHireMeCarousel from "@/components/organisms/WhyHireMeCarousel";
+import { useTranslations } from "next-intl";
 
 //Seccion completa de ¿Por Qué Contratarme?. Utiliza el Header de Seccion y el carrousel definido.
 export default function WhyHireMeSection() {
+  const t = useTranslations("Main.WhyHireMe");
   return (
     <div className="flex flex-col gap-y-4">
       <SectionHeader
-        title="¿Por Qué Contratarme?"
-        description="Más allá de mis habilidades técnicas, esta sección destaca el valor único que aporto a cada proyecto y equipo. Descubre mis cualidades clave como desarrollador y cómo mi enfoque proactivo y pensamiento integral pueden beneficiar a tu empresa."
+        title={t("title")}
+        description={t("description")}
       ></SectionHeader>
       <div className="flex items-center justify-center pr-5 pl-5 align-middle">
         <WhyHireMeCarousel />
