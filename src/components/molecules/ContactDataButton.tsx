f4023d865bd2fe126d6391fb6c2acc2b04905a13
Wilmer Soto <wsoto04@gmail.com>
2026-03-12 16:44:04 -0500

    feat: enhance Hero section with dynamic translations and detailed introduction

--- src/components/molecules/ContactDataButton.tsx
+++ src/components/molecules/ContactDataButton.tsx
@@ -10,27 +10,29 @@ import Link from "next/link";
 import { FaLinkedin } from "react-icons/fa";
 import { Mail } from "lucide-react";
 import { Button } from "@/components/ui/button";
-
-const ContactData = [
-  {
-    href: "https://www.linkedin.com/in/wilmer-soto-04346a367/",
-    title: "Linkedin",
-    Icon: FaLinkedin,
-  },
-  { href: "mailto:wsoto04@gmail.com", title: "Enviar email", Icon: Mail },
-];
+import { useTranslations } from "next-intl";
 
 // Se usa DropdownMenu con un array para los datos de contacto. Esto para el boton de "Mira mis datos de contacto!"
 export default function ContactDataButton() {
+  const t = useTranslations("Main.Hero.button");
+  const ContactData = [
+    {
+      href: "https://www.linkedin.com/in/wilmer-soto-04346a367/",
+      title: "Linkedin",
+      Icon: FaLinkedin,
+    },
+    { href: "mailto:wsoto04@gmail.com", title: t("email"), Icon: Mail },
+  ];
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button className="bg-yellow-500 text-black hover:bg-yellow-600">
-          Mira mis datos de contacto! →
+          {t("title")}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuLabel>Contacto</DropdownMenuLabel>
+        <DropdownMenuLabel>{t("subtitle")}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         {ContactData.map((entry, index) => (
           <DropdownMenuItem key={index}>
