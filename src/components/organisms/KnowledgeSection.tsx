0d48817dbeba62e806ac2be3b56fa1440a3d8672
Wilmer Soto <wsoto04@gmail.com>
2026-03-12 16:54:34 -0500

    feat: enhance Knowledge section with localized titles and descriptions

--- src/components/organisms/KnowledgeSection.tsx
+++ src/components/organisms/KnowledgeSection.tsx
@@ -8,49 +8,49 @@ import {
   Network,
   ListCheck,
 } from "lucide-react";
-
-const knowledgeData = [
-  {
-    title: "Desarrollo Web",
-    description: "Next.js, Tailwind CSS, HTML, CSS, JavaScript/TypeScript",
-    Icon: MonitorSmartphone,
-  },
-  {
-    title: "Desarrollo Backend",
-    description: "Spring Boot, APIs REST, Java",
-    Icon: Server,
-  },
-  {
-    title: "Bases de Datos",
-    description: "PostgreSQL, DBeaver, SQL",
-    Icon: Database,
-  },
-  {
-    title: "Herramientas de Desarrollo y DevOps",
-    description:
-      "Git, GitHub, Docker, GitHub Actions, Docker Compose, Azure DevOps",
-    Icon: Workflow,
-  },
-  {
-    title: "Gestión de APIs e Integración",
-    description: "RESTful APIs, GraphQL, Postman",
-    Icon: Network,
-  },
-  {
-    title: "Metodologia",
-    description: "Agile, Scrum, Trello",
-    Icon: ListCheck,
-  },
-];
+import { useTranslations } from "next-intl";
 
 //Seccion completa de Conocimientos. Utiliza el Header de Seccion y la card definida en moleculas.
 export default function KnowledgeSection() {
+  const t = useTranslations("Main.Knowledge");
+  const knowledgeData = [
+    {
+      title: t("cards.web"),
+      description:
+        "Next.js, Tailwind CSS, HTML, CSS, JavaScript/TypeScript, Microsoft Power Apps/Power Automate",
+      Icon: MonitorSmartphone,
+    },
+    {
+      title: t("cards.backend"),
+      description: "Spring Boot, APIs REST, Java, Python",
+      Icon: Server,
+    },
+    {
+      title: t("cards.db"),
+      description: "PostgreSQL, DBeaver, SQL",
+      Icon: Database,
+    },
+    {
+      title: t("cards.devops"),
+      description:
+        "Git, GitHub, Docker, GitHub Actions, Docker Compose, Azure DevOps",
+      Icon: Workflow,
+    },
+    {
+      title: "APIs",
+      description: "RESTful APIs, GraphQL, Postman",
+      Icon: Network,
+    },
+    {
+      title: t("cards.methodologies"),
+      description: "Agile, Scrum, Trello",
+      Icon: ListCheck,
+    },
+  ];
+
   return (
     <div className="flex flex-col gap-y-4 text-wrap">
-      <SectionHeader
-        title="Mis conocimientos"
-        description="Explora aquí un resumen de mis habilidades y las tecnologías clave que domino como desarrollador de software."
-      />
+      <SectionHeader title={t("title")} description={t("description")} />
       <div className="grid grid-cols-1 grid-rows-2 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {knowledgeData.map((card, index) => (
           <KnowledgeCard
